7a84cef87790d98f3a4accee55a1688653a375a8
Vladislav Lenivenko <vladislav.lenivenko@dataart.com>
2025-04-18 18:47:07 +0300

    api fix

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 export async function getEntities() {
-  const response = await fetch('https://hoodie-api.azurewebsites.net/WeatherForecast');
+  const response = await fetch('https://hoodie-api.azurewebsites.net/api/WeatherForecast');
   if (!response.ok) throw new Error('Failed to fetch');
   return response.json();
 }
